6689f079212fb7e0e0cbbf4fa729e7b2992aa5c4
Derek Slagle <slagle.derek@gmail.com>
2016-08-31 22:53:47 -0400

    added html lint output artifact

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,29 @@
 var gulp = require("gulp");
+var style = require("gulp-jshint-html-reporter")
 var lint = require("gulp-jshint");
 var debug = require("gulp-debug");
 
 var lintConfig = require("./package.json").jshintConfig;
 
-gulp.task("lint", function() {
+gulp.task("lint-report", function(cb) {
     gulp.src([
             "server.js",
         ])
-        .pipe(debug())
         .pipe(lint(lintConfig))
-        .pipe(lint.reporter('default'))
-        .pipe(lint.reporter('fail'));
+        .pipe(lint.reporter(
+            'gulp-jshint-html-reporter',
+            {
+                filename: __dirname + "/test/lint.html",
+                createMissingFolders: false
+            }
+        ))
+        .pipe(cb());
 });
 
-gulp.task("test", function() {
-    
+gulp.task("lint-check", function() {
+    gulp.src([
+            "server.js",
+        ])
+        .pipe(lint(lintConfig))
+        .pipe(lint.reporter('fail'));
 });
